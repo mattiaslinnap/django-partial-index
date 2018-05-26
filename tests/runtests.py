405cfaf24a96a1243c7144a601724afe54ce621b
Mattias Linnap <mattias@linnap.com>
2018-05-26 13:13:49 +0200

    runtest.py now allows --skip-postgresql or --skip-sqlite

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+import argparse
 from os.path import abspath, dirname, join
 import subprocess
 import sys
@@ -11,16 +12,24 @@ TESTS_DIR = join(REPO_DIR, 'tests')
 RUNNER_PY = join(TESTS_DIR, 'runner.py')
 
 
-def main():
-    print('Testing with PostgreSQL...', file=sys.stderr)
-    ret1 = subprocess.call([RUNNER_PY, '--db', 'postgresql'])
-    print('Done testing with PostgreSQL.', file=sys.stderr)
+def main(args):
+    exitcodes = []
+    if not args.skip_postgresql:
+        print('Testing with PostgreSQL...', file=sys.stderr)
+        exitcodes.append(subprocess.call([RUNNER_PY, '--db', 'postgresql']))
+        print('Done testing with PostgreSQL.', file=sys.stderr)
     print('', file=sys.stderr)
-    print('Testing with SQLite...', file=sys.stderr)
-    ret2 = subprocess.call([RUNNER_PY, '--db', 'sqlite'])
-    print('Done testing with SQLite.', file=sys.stderr)
-    sys.exit(ret1 or ret2)
+    if not args.skip_sqlite:
+        print('Testing with SQLite...', file=sys.stderr)
+        exitcodes.append(subprocess.call([RUNNER_PY, '--db', 'sqlite']))
+        print('Done testing with SQLite.', file=sys.stderr)
+    # Exit with 0 if all non-skipped tests did the same.
+    sys.exit(max(exitcodes) or 0)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Runs tests.')
+    parser.add_argument('--skip-postgresql', default=False, action='store_true')
+    parser.add_argument('--skip-sqlite', default=False, action='store_true')
+    args = parser.parse_args()
+    main(args)
